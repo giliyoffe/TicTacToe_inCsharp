9f08674225c2f5a9285fb94df04485dc6be6689a
giliyoffe <gili26yoffe@gmail.com>
2017-10-26 13:04:24 +0200

    Add files via upload

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Spieler = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Spieler)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox8, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox6, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox9, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox5, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(127, 71);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(320, 319);
+            this.tableLayoutPanel1.TabIndex = 9;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint_1);
+            // 
+            // Spieler
+            // 
+            this.Spieler.Image = global::TicTacToe.Resource1.OOO;
+            this.Spieler.InitialImage = global::TicTacToe.Resource1.OOO;
+            this.Spieler.Location = new System.Drawing.Point(524, 75);
+            this.Spieler.Name = "Spieler";
+            this.Spieler.Size = new System.Drawing.Size(100, 100);
+            this.Spieler.TabIndex = 12;
+            this.Spieler.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackgroundImage = global::TicTacToe.Resource1.BackgroundImage;
+            this.pictureBox8.Location = new System.Drawing.Point(218, 218);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox8.TabIndex = 9;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackgroundImage = global::TicTacToe.Resource1.BackgroundImage;
+            this.pictureBox7.Location = new System.Drawing.Point(4, 218);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = global::TicTacToe.Resource1.BackgroundImage;
+            this.pictureBox6.Location = new System.Drawing.Point(111, 218);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::TicTacToe.Resource1.BackgroundImage;
+            this.pictureBox3.Location = new System.Drawing.Point(111, 111);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::TicTacToe.Resource1.BackgroundImage;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::TicTacToe.Resource1.BackgroundImage;
+            this.pictureBox2.Location = new System.Drawing.Point(4, 111);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::TicTacToe.Resource1.BackgroundImage;
+            this.pictureBox4.Location = new System.Drawing.Point(218, 4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackgroundImage = global::TicTacToe.Resource1.BackgroundImage;
+            this.pictureBox9.Location = new System.Drawing.Point(218, 111);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox9.TabIndex = 11;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = global::TicTacToe.Resource1.BackgroundImage;
+            this.pictureBox5.Location = new System.Drawing.Point(111, 4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 661);
+            this.Controls.Add(this.Spieler);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Spieler)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox Spieler;
+
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+            //jenach dem, welcher Spieler dran ist, wird das passende Symbol gesetzt
+            ((PictureBox)sender).Image = Spieler.Image;
+            ((PictureBox)sender).Enabled = false;
+            if ((string)Spieler.Image.Tag == "Kreuz")
+            {
+                Spieler.Image = Resource1.OOO;
+                Spieler.Image.Tag = "OOO";
+                ((PictureBox)sender).Image.Tag = "Kreuz";
+            }
+            else
+            {
+                Spieler.Image = Resource1.Kreuz;
+                Spieler.Image.Tag = "Kreuz";
+                ((PictureBox)sender).Image.Tag = "OOO";
+
+            }
+            //hat einer gewonnen?
+            TableLayoutPanelCellPosition p = tableLayoutPanel1.GetPositionFromControl((Control)sender);
+            int[,] vek = { { 1, 2 }, { -1, 1 }, { -2, -1 } };
+            bool win = false;
+            PictureBox s = (PictureBox)sender;
+            //horizontal prüf
+            win = win ||
+                ((string)s.Image.Tag
+                == (string)((PictureBox)tableLayoutPanel1.GetControlFromPosition(p.Column + vek[p.Column, 0], p.Row)).Image.Tag
+                &&
+                (string)s.Image.Tag
+                == (string)((PictureBox)tableLayoutPanel1.GetControlFromPosition(p.Column + vek[p.Column, 1], p.Row)).Image.Tag
+                );
+            //vertikal prüf
+            win = win ||
+                ((string)s.Image.Tag
+                == (string)((PictureBox)tableLayoutPanel1.GetControlFromPosition(p.Column, p.Row + vek[p.Row, 0])).Image.Tag
+                &&
+                (string)s.Image.Tag
+                == (string)((PictureBox)tableLayoutPanel1.GetControlFromPosition(p.Column, p.Row + vek[p.Row, 1])).Image.Tag
+                );
+            //backslash
+            win = win || ((p.Column == p.Row) &&
+               ((string)s.Image.Tag
+               == (string)((PictureBox)tableLayoutPanel1.GetControlFromPosition(p.Column + vek[p.Column, 0], p.Row + vek[p.Row, 0])).Image.Tag
+               &&
+               (string)s.Image.Tag
+               == (string)((PictureBox)tableLayoutPanel1.GetControlFromPosition(p.Column + vek[p.Column, 1], p.Row + vek[p.Row, 1])).Image.Tag
+               ));
+
+            if (win)
+            {
+                MessageBox.Show("Einer hat gewonnen");
+                return;
+            }
+            //unentschieden
+            foreach (Control ctrl in tableLayoutPanel1.Controls)
+            {
+                if (ctrl.Enabled)
+                {
+                    return;
+                }
+            }
+            MessageBox.Show("Game over! Unentschieden!");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Spieler.Image.Tag = "OOO";
+        }
+    }
+}
